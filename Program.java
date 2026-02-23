cb0bd0fa80b477aa55f6827a62b73bdff69b4bbe
SAS <87268224+SAStephen421@users.noreply.github.com>
2026-02-23 08:03:22 +0530

    Updated

--- Program.java
+++ Program.java
@@ -1,4 +1,10 @@
-public class Program {
+/**
+ * Printing OOPS
+ * @author Niranjan C I
+ * @version 1.1
+ */
+
+public class Program{
     public static void main(String[] args) {
         System.out.println(String.join(" ", "*****", " ","*****", " ", "*****", " ", "*****"));
         System.out.println(String.join(" ", "*   *", " ", "*   *", " ", "*   *", " ", "*    "));
